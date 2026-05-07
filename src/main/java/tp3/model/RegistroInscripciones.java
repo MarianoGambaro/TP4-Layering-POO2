719fa3137b41a0d9d3726014a5f5fb59cce4a160
MarianoGambaro <martinezgambaromariano@gmail.com>
2026-05-07 12:50:29 -0600

    jdeps

--- src/main/java/tp3/model/RegistroInscripciones.java
+++ src/main/java/tp3/model/RegistroInscripciones.java
@@ -3,3 +3,5 @@ package tp3.model;
 public interface RegistroInscripciones {
     void registrarInscripcion(Inscripcion inscripcion);
 }
+
+//jdeps -verbose:package -p tp3.persistencia -p tp3.model -p tp3.vista target\classes
